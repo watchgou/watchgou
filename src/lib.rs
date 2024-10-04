f1819f46953000e93b7e7bcf81fe16368c3c1723
jon <zjon234@gmail.com>
2024-10-04 22:10:32 +0800

    encode base64

--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,19 @@ use std::hash::{DefaultHasher, Hash, Hasher};
 use base64::{engine::general_purpose::STANDARD, Engine};
 use wasm_bindgen::prelude::wasm_bindgen;
 
+///
+///     字符串计算hash
+///
 #[wasm_bindgen]
 pub fn hash_64(c_str: &str) -> u64 {
     let mut hasher = DefaultHasher::new();
     c_str.hash(&mut hasher);
     hasher.finish()
 }
+
 ///
-///  import ./watchgou
-/// encode_base64();
-///
+///     import ./watchgou
+///     encode_base64();
 ///
 #[wasm_bindgen]
 pub fn encode_base64(bytes: &[u8]) -> String {
