7cd809d092ba987f15fedd49c0e7ecddc8444b99
jon <zjon234@gmail.com>
2024-10-04 22:08:53 +0800

    encode base64

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::hash::{DefaultHasher, Hash, Hasher};
 
+use base64::{engine::general_purpose::STANDARD, Engine};
 use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
@@ -7,4 +8,14 @@ pub fn hash_64(c_str: &str) -> u64 {
     let mut hasher = DefaultHasher::new();
     c_str.hash(&mut hasher);
     hasher.finish()
+}
+///
+///  import ./watchgou
+/// encode_base64();
+///
+///
+#[wasm_bindgen]
+pub fn encode_base64(bytes: &[u8]) -> String {
+    // direct3D
+    STANDARD.encode(bytes)
 }
