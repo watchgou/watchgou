4f0a5fe68a6df33c522b4b820e33f2313d5117c8
jon <zjon234@gmail.com>
2024-10-04 21:24:45 +0800

    calculate hash

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
+use std::hash::{DefaultHasher, Hash, Hasher};
+
 use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
 pub fn hash_64(c_str: &str) -> u64 {
-    12
+    let mut hasher = DefaultHasher::new();
+    c_str.hash(&mut hasher);
+    hasher.finish()
 }
